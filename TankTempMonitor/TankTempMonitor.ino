606fa08797115690f0da8095b7d253d2779e1174
Sam Faull <samz5991@hotmail.co.uk>
2018-01-06 20:04:17 +0000

    Tidy up plus added software serial to send data to esp (compiles but untested)

--- TankTempMonitor/TankTempMonitor.ino
+++ TankTempMonitor/TankTempMonitor.ino
@@ -1,35 +1,97 @@
 // Tank temp monitor for Arduino Nano - Sam Faull
 
-/* ADJUSTABLE PARAMETERS, please alter the values in this section to your preference */
-#define lowerLimit 25   // temp (in deg C) at which LEDs go blue
-#define upperLimit 35   // temp (in deg C) at which LEDs go red
+#include <SoftwareSerial.h>
 
-#define beepQuantity 5  // the number of times the buzzer beep 
-#define beepDelay 180   // the delay (in seconds) before the alarm sounds
-#define refreshRate 100 // how often (in milliseconds) the sensors will refresh 
-                        //   this is also how long the buzzer will beep for 
-                        //   (time on = refreshRate, time off = refreshRate) 
+/* ADJUSTABLE PARAMETERS, please alter the values in this section to your preference */
+#define lowerLimit      25   // temp (in deg C) at which LEDs go blue
+#define upperLimit      35   // temp (in deg C) at which LEDs go red
+
+#define beepQuantity    5       // the number of times the buzzer beep 
+#define beepDelay       18000   // 3 mins -> the delay before the alarm sounds
+#define ALARM_PERIOD    500     // 500ms
+#define PUBLISH_PERIOD  12000  // 2 mins = 2 * 60 * 1000
+#define UPDATE_PERIOD   1000     // how often (in milliseconds) the sensors will refresh 
+                                //   this is also how long the buzzer will beep for 
+                                //   (time on = refreshRate, time off = refreshRate) 
+
+#define SENSOR_COUNT  4
+#define BUFFER_SIZE   10
+
+SoftwareSerial esp(10, 11); // RX, TX
+
 
 // Global variables
 int cnt = beepDelay + (2*beepQuantity) + 1;    // global counter variable
+bool alarm_enabled = false;
+int alarm_count = 0;
+bool standby = false;
 
-float tempA,tempB,tempC,tempD = 0;
+int readArray[SENSOR_COUNT][BUFFER_SIZE] = {};
+float temp[SENSOR_COUNT] = {0,0,0,0};
+
+unsigned long runTime         = 0,
+              alarmTimer      = 0,
+              updateTimer     = 0,
+              publishTimer    = 0;
 
 void setup()
 {
-  //Serial.begin(9600);  //Start the serial connection with the computer
+  Serial.begin(9600);  //Start the serial connection with the computer
+  esp.begin(9600);   // start a software serial to talk to the ESP
   init_IO();  // set up outputs and inputs
   LED_test(); // run LED test sequence
-
+  setTimer(&updateTimer);
+  setTimer(&publishTimer);
+  Serial.println("Ready...");
 }    
                    
 /////////////////////////////////////main code//////////////////////////////////////////////////////////
 void loop()           
 {
-  read_temp();
-  serial_debug();
-  set_LED_state();
-  delay(refreshRate);    // waiting for a short period
+  /*
+  if (esp.available() > 0) 
+  { 
+    String incoming = esp.readString(); // read the incoming string:  
+    Serial.print(" I received:");   
+    Serial.println(incoming);
+  }
+  */
+
+  /* Periodically read the inputs */
+  if (timerExpired(updateTimer, UPDATE_PERIOD)) // check for button press periodically
+  {
+    setTimer(&updateTimer);  // reset timer
+    read_temp();
+    //serial_debug();
+    set_LED_state();
+  }
+
+  /* Periodically publish readings */
+  if (timerExpired(publishTimer, PUBLISH_PERIOD)) // check for button press periodically
+  {
+    setTimer(&publishTimer);  // reset timer
+    
+    Serial.println("Publish temperature");
+    // upload to te internet
+    String cmd;
+    /* send the ID of the sensor followed by the temperature (comma seperated) */
+    cmd = String(temp[0]) + "," + String(temp[1]) + "," + String(temp[2]) + "," + String(temp[3]);
+    esp.println(cmd);
+    Serial.println(cmd);
+  }
+
+  /* Periodically publish readings */
+  if (timerExpired(alarmTimer, ALARM_PERIOD)) // check for button press periodically
+  {
+    setTimer(&alarmTimer);  // reset timer
+    if (alarm_enabled && !standby)
+    {
+      alarm_count++;
+      if (alarm_count > beepDelay*2)
+        alarm();
+    }
+  }
+      
 }
 
 void init_IO(void)
@@ -74,138 +136,121 @@ void LED_test(void)
 
 void read_temp(void)
 {
-  int i;
   static int addr = 0;
-  static int readArrayA[10] = {};
-  static int readArrayB[10] = {};
-  static int readArrayC[10] = {};
-  static int readArrayD[10] = {};
-  int readingA;
-  int readingB;
-  int readingC;
-  int readingD;
+  float voltage[SENSOR_COUNT];
+  static long movingTotal[SENSOR_COUNT];
 
-  // get adc reading from temp sensor  
-  readArrayA[addr] = analogRead(0);
-  readArrayB[addr] = analogRead(1);
-  readArrayC[addr] = analogRead(2);
-  readArrayD[addr] = analogRead(3);
+  Serial.println("Read sensors:");
+    
+  // get adc reading from temp sensors  
+  for (int i=0; i>SENSOR_COUNT; i++)
+  {
+    // temp is our moving average, so...
+    // subtract the temperature value thats about to be wiped from the array
+    movingTotal[i] -= readArray[i][addr];
+    // read in the new value
+    readArray[i][addr] = analogRead(i);
+    // add said new value to the moving total
+    movingTotal[i] += readArray[i][addr];
+    
+    // convert the moving total adc reading to voltage (for 3.3v arduino use 3.3)
+    voltage[i] = (movingTotal[i]/BUFFER_SIZE) * 5.0;
+    voltage[i] /= 1024.0; 
+
+    // calculate temp from voltage
+    temp[i] = (voltage[i] - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset to degrees ((voltage - 500mV) times 100)
+
+    if (i==1)
+      temp[i] -= 6.3 ;  // 6 deg offset because this sensor is slightly out
+
+    Serial.println(temp[i]);
+  }
 
   // increment the array address pointer (circular buffer)
-  if (addr >= 9)
+  if (addr >= BUFFER_SIZE-1)
     addr=0;
   else
     addr++;
 
-  for (i=0; i<=9; i++)
-  {
-    readingA += readArrayA[i];
-    readingB += readArrayB[i];
-    readingC += readArrayC[i];
-    readingD += readArrayD[i];
-  }
-  
-  readingA = readingA/10;
-  readingB = readingB/10;
-  readingC = readingC/10;
-  readingD = readingD/10;
-
-  // convert adc reading to voltage (for 3.3v arduino use 3.3)
-  float voltageA = readingA * 5.0;
-  voltageA /= 1024.0; 
-  float voltageB = readingB * 5.0;
-  voltageB /= 1024.0; 
-  float voltageC = readingC * 5.0;
-  voltageC /= 1024.0; 
-  float voltageD = readingD * 5.0;
-  voltageD /= 1024.0; 
-    
-  // calculate temp from voltage
-  tempA = (voltageA - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset to degrees ((voltage - 500mV) times 100)
-  tempB = ((voltageB - 0.5) * 100) - 6.3 ;  // 6 deg offset because this sensor is slightly out
-  tempC = (voltageC - 0.5) * 100 ;
-  tempD = (voltageD - 0.5) * 100 ;
 }
 
 void serial_debug(void)
 {  
   // print temp to serial
-  Serial.print(tempA); Serial.print(" deg C \t");
-  Serial.print(tempB); Serial.print(" deg C \t");
-  Serial.print(tempC); Serial.print(" deg C \t");
-  Serial.print(tempD); Serial.println(" deg C \t");
+  for (int i=0; i<SENSOR_COUNT; i++)
+    Serial.print(temp[0]); Serial.print(" deg C \t");
 }
 
 void set_LED_state(void)
 {
-///////////////////////////////////conditions for sensor A /////////////////////////////////////////
- if (tempA <= lowerLimit)
+///////////////////////////////////conditions for sensor 0 /////////////////////////////////////////
+ if (temp[0] <= lowerLimit)
  {
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
  }
- else if (tempA > lowerLimit && tempA < upperLimit)
+ else if (temp[0] > lowerLimit && temp[0] < upperLimit)
  {
   digitalWrite(2, HIGH);
   digitalWrite(3, LOW);
  }
- else if (tempA >= upperLimit)
+ else if (temp[0] >= upperLimit)
  {
   digitalWrite(2, LOW);
   digitalWrite(3, HIGH);
  }
 
-///////////////////////////////////conditions for sensor B ///////////////////////////////////////// 
- if (tempB <= lowerLimit) 
+///////////////////////////////////conditions for sensor 1 ///////////////////////////////////////// 
+ if (temp[1] <= lowerLimit) 
  {
   digitalWrite(4, LOW);
   digitalWrite(5, LOW);
   digitalWrite(10, LOW);
-  cnt = beepDelay + (2*beepQuantity) + 1;
+  alarm_enabled = true; 
  }
- else if (tempB > lowerLimit && tempB < upperLimit)
+ else if (temp[1] > lowerLimit && temp[1] < upperLimit)
  {
-  alarm();
   digitalWrite(4, HIGH);
   digitalWrite(5, LOW);
+  alarm_enabled = false;
  }
- else if (tempB >= upperLimit)
+ else if (temp[1] >= upperLimit)
  {
   digitalWrite(4, LOW);
   digitalWrite(5, HIGH);
   digitalWrite(10, LOW);
-  cnt = 0;
+  alarm_enabled = false;
  }
 
- ///////////////////////////////////conditions for sensor C /////////////////////////////////////////
- if (tempC <= lowerLimit)
+ ///////////////////////////////////conditions for sensor 2 /////////////////////////////////////////
+ if (temp[2] <= lowerLimit)
  {
   digitalWrite(6, LOW);
   digitalWrite(7, LOW);
  }
- else if (tempC > lowerLimit && tempC < upperLimit)
+ else if (temp[2] > lowerLimit && temp[2] < upperLimit)
  {
   digitalWrite(6, HIGH);
   digitalWrite(7, LOW);
  }
- else if (tempC >= upperLimit)
+ else if (temp[2] >= upperLimit)
  {
   digitalWrite(6, LOW);
   digitalWrite(7, HIGH);
  }
 
- ///////////////////////////////////conditions for sensor D /////////////////////////////////////////
- if (tempD <= lowerLimit)
+ ///////////////////////////////////conditions for sensor 3 /////////////////////////////////////////
+ if (temp[3] <= lowerLimit)
  {
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
  }
- else if (tempD > lowerLimit && tempD < upperLimit)
+ else if (temp[3] > lowerLimit && temp[3] < upperLimit)
  {
   digitalWrite(8, HIGH);
   digitalWrite(9, LOW);
  }
- else if (tempD >= upperLimit)
+ else if (temp[3] >= upperLimit)
  {
   digitalWrite(8, LOW);
   digitalWrite(9, HIGH);
@@ -214,21 +259,45 @@ void set_LED_state(void)
 
 void alarm(void)
 {
-// Buzzer will turn on and off at the refresh rate when the temp dips bellow 45 degrees
-// It will do this 5 times and then stop
+  // Buzzer will turn on and off at the refresh rate when the temp dips bellow 45 degrees
+  // It will do this 5 times and then stop
+  
+  static bool alarm_state = 0;
+  static int cycles = 0;
+  
+  alarm_state != alarm_state;
 
-  if (cnt <= (beepDelay + (2*beepQuantity) -1))
-  {
-    cnt++;
-    if (cnt >= beepDelay)
-    {
-      if (cnt%2)
-        digitalWrite(10, HIGH);
-      else
-        digitalWrite(10, LOW);
-    }
+  if (alarm_state)
+    digitalWrite(10, HIGH);
   else
-      digitalWrite(10, LOW);
+  {
+    digitalWrite(10, LOW);
+    cycles++;
   }
+
+  if (cycles > 10)
+  {
+    alarm_enabled = false;
+    alarm_count = 0;
+  }
+
+}
+
+/* pass this function a pointer to an unsigned long to store the start time for the timer */
+void setTimer(unsigned long *startTime)
+{
+  runTime = millis();    // get time running in ms
+  *startTime = runTime;  // store the current time
+}
+
+/* call this function and pass it the variable which stores the timer start time and the desired expiry time 
+   returns true fi timer has expired */
+bool timerExpired(unsigned long startTime, unsigned long expiryTime)
+{
+  runTime = millis(); // get time running in ms
+  if ( (runTime - startTime) >= expiryTime )
+    return true;
+  else
+    return false;
 }
 
